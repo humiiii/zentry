d5f344c5194d5c4d11d5ef16417fac67c2b86907
Muhammad Umaid Rather <107248121+humiiii@users.noreply.github.com>
2025-05-27 19:31:54 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="text-5xl text-orange-500 font-bold">
+      Welcome umaidRather!
+    </div>
+  );
+};
+
+export default App;
