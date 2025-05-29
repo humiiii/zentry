a8df5e03943c193c918e7b84612b4b21b405e68d
Muhammad Umaid Rather <107248121+humiiii@users.noreply.github.com>
2025-05-29 13:32:44 +0530

    added hero section and react icons

--- src/components/Hero.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Hero = () => {
-  return <div className="text-5xl font-bold animated-title">Hero</div>;
-};
-
-export default Hero;
---
+++ src/section/Hero.jsx
@@ -0,0 +1,99 @@
+import React, { useRef, useState } from "react";
+import Button from "../components/Button";
+import { TiLocationArrow } from "react-icons/ti";
+
+const Hero = () => {
+  const [currentIndex, setCurrentIndex] = useState(1);
+  const [hasClicked, setHasClicked] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedVideos, setLoadedVideos] = useState(0);
+
+  const totalVideos = 3;
+  const nextVideoRef = useRef(null);
+
+  const upcommintVideoIndex = (currentIndex % totalVideos) + 1;
+
+  const handleVideoLoad = () => {
+    setLoadedVideos((prev) => prev + 1);
+  };
+
+  const handleMiniVideoClick = () => {
+    setHasClicked(true);
+
+    setCurrentIndex(upcommintVideoIndex);
+  };
+
+  const getVideoSrc = (index) => `/zentry/videos/hero-${index}.mp4`;
+
+  return (
+    <div className="relative h-dvh w-screen overflow-x-hidden">
+      <div
+        id="video-frame"
+        className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75"
+      >
+        <div className="">
+          <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
+            <div
+              onClick={handleMiniVideoClick}
+              className="origin-center scale-50 hover:scale-100 transition-all duration-500 ease-in opacity-0 hover:opacity-100"
+            >
+              <video
+                ref={nextVideoRef}
+                src={getVideoSrc(upcommintVideoIndex)}
+                loop
+                muted
+                id="current-video"
+                className="size-64 origin-center scale-150 object-cover object-center"
+                onLoadedData={handleVideoLoad}
+              />
+            </div>
+          </div>
+          <video
+            id="next-video"
+            src={getVideoSrc(currentIndex)}
+            loop
+            muted
+            className="absolute-center absolute z-20 size-64 object-cover object-center invisible"
+            onLoadedData={handleVideoLoad}
+          />
+          <video
+            src={getVideoSrc(
+              currentIndex === totalVideos - 1 ? 1 : currentIndex
+            )}
+            loop
+            autoPlay
+            muted
+            className="absolute top-0 left-0 size-full object-cover object-center"
+            onLoadedData={handleVideoLoad}
+          />
+        </div>
+        <h1 className="absolute z-40 bottom-5 right-5 text-blue-75 hero-heading special-font">
+          g<b>a</b>ming
+        </h1>
+        <div className="absolute left-0 top-0 z-40 size-full">
+          <div className="mt-24 px-5 sm:px-10">
+            <h1 className="special-font hero-heading text-blue-100">
+              redefi<b>n</b>e
+            </h1>
+            <p className="max-w-64 font-[robert-regular] text-blue-100 mb-5">
+              Enter the Metagame
+              <br />
+              Unleash the Play Economy
+            </p>
+            <Button
+              id={"watch-trailer"}
+              title={"Watch Trailer"}
+              leftIcon={<TiLocationArrow />}
+              containerClass={"!bg-yellow-300 flex-center gap-1"}
+            />
+          </div>
+        </div>
+      </div>
+      <h1 className="absolute bottom-5 right-5 text-black hero-heading special-font">
+        g<b>a</b>ming
+      </h1>
+    </div>
+  );
+};
+
+export default Hero;
